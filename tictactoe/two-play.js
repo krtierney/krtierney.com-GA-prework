cb77061ede4b3ff53c75f48cf607199db3a24c03
Kaitlyn Tierney <k@krtierney.com>
2016-07-03 10:52:27 +0100

    feat: added single and two-player modes to tic tac toe game

--- tictactoe/two-play.js
+++ tictactoe/two-play.js
@@ -1,9 +1,9 @@
 var winConditions = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
 var squares = document.getElementsByTagName("li");
 var resetButton = document.getElementsByTagName("button")[0];
+var playerTurn = 0;
 var isX = [];
 var isO = [];
-var opponentPick = [];
 var isWinner = false;
 
 for (var i=0; i<squares.length; i++) {
@@ -14,15 +14,29 @@ for (var i=0; i<squares.length; i++) {
 
 function playerMove() {
   if (this.innerHTML === "") {
-    this.innerHTML = "x";
-    isX.push(this.value);
-    winCheck(isX);
-    if (isWinner === true) {
-      alert("You win! Click OK to play again.");
-      reset(); 
+    if (playerTurn % 2 === 0) {
+      this.innerHTML = "x";
+      isX.push(this.value);
+      winCheck(isX);
+      if (isWinner === true) {
+        alert("The X has it. Click OK to play again.");
+
+        reset(); 
+      }
     } else {
-      setTimeout(opponent, 300);  
-    } 
+      this.innerHTML = "<p>o</p>";
+      isO.push(this.value);
+      winCheck(isO);
+      if (isWinner === true) {
+        alert("O wins! Click OK to play again.");
+        reset(); 
+      }
+    }
+    playerTurn++;
+    if (playerTurn === 9) {
+      alert("It's a draw. Click OK to play again");
+    }
+
   }
 }
 
@@ -42,6 +56,7 @@ function winCheck(playerArray) {
 resetButton.addEventListener('click', reset);
 
 function reset() {
+  playerTurn = 0;
   isX = [];
   isO = [];
   isWinner = false;
@@ -49,29 +64,3 @@ function reset() {
     squares[i].innerHTML = "";
   }
 }
-
-//Computer plays back with random selections
-function opponent() {
-  for (var i=0; i<squares.length; i++) {
-    if (squares[i].innerHTML === "") {
-      opponentPick.push(i);
-    } 
-  }
-
-  if (opponentPick.length === 0){
-    alert("It's a draw. Click OK to play again");
-    reset();
-  } else {  
-    i = opponentPick[Math.floor(Math.random() * (opponentPick.length))];
-    squares[i].innerHTML = "<p>o</p>";
-    isO.push(squares[i].value);
-    winCheck(isO);
-    if (isWinner === true) {
-      alert("You lost. Ouch. Click OK to play again.");
-      reset(); 
-    }
-  }
-  opponentPick = [];
-}
-
-
